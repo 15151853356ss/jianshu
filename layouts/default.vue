075b93f5f401cbabb6875ea77b8bf2e91a012f9b
yss <2677683166@qq.com>
2018-04-18 19:09:09 +0800

    新增登录界面

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,13 +5,6 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-Vue.use(BootstrapVue)
-// 首先引入bootstrap.css文件，加载bootstrap样式
-import 'assets/css/bootstrap.css'
-// 引入bootstrap-vue.css文件
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 export default {
 
 }
